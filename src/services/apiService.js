78a8baed3522694d660039b77631376155c7b675
henry8812 <henry8812@gmail.com>
2023-08-29 10:42:38 -0500

    merge

---
+++ src/services/apiService.js
@@ -0,0 +1,13 @@
+/**
+ * @author Henry A. Gomez M.
+ * @
+ */
+const apiService = {
+    return {
+        getAgents = new Promise( (resolve) => {
+            resolve(null)
+        })
+    }
+}
+
+export default apiService;
